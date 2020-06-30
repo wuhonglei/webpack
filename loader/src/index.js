89465d10169ec800bad87f5f5d5f049952b14a27
wuhonglei <1017368065@qq.com>
2020-06-30 13:35:56 +0800

    新增了 loader 示例配置

---
+++ loader/src/index.js
@@ -0,0 +1,7 @@
+import { cube } from './math.js';
+
+import './base.css';
+console.info(cube(3));
+
+var person = {};
+console.info(`${person?.name}`);
---
+++ loader/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader']
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: [{
+                loader: 'babel-loader',
+            }]
+        }]
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
